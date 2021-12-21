9c2f9063202ba6525008db1bc94bf8703e18ff06
shashi918 <sr0298037@gmail.com>
2021-12-21 21:33:25 +0530

    Median of Two Sorted Arrays

---
+++ C++/Median_of_Two_Sorted_Arrays.cpp
@@ -0,0 +1,105 @@
+//Median of Two Sorted Arrays
+/*
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+*/
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <cmath>
+#include <math.h>
+#include <vector>
+using namespace std;
+double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
+  if (nums1.size() > nums2.size()) {
+    vector<int> temp = nums1;
+    nums1 = nums2;
+    nums2 = temp;
+  }
+  int low = 0;
+  int high = nums1.size();
+  int total = nums1.size() + nums2.size();
+  while (low <= high) {
+    int nums1partition = (low + high) / 2;
+    int nums2partition = (total + 1) / 2 - nums1partition;
+
+    int nums1left = (nums1partition == 0) ? INT_MIN : nums1[nums1partition - 1];
+    int nums1right =
+        (nums1partition == nums1.size()) ? INT_MAX : nums1[nums1partition];
+    int nums2left = (nums2partition == 0) ? INT_MIN : nums2[nums2partition - 1];
+    int nums2right =
+        (nums2partition == nums2.size()) ? INT_MAX : nums2[nums2partition];
+
+    if (nums1left <= nums2right && nums2left <= nums1right) {
+      if (total % 2 == 0) {
+        return (max(nums1left,nums2left)+min(nums1right,nums2right))/2.0;
+      } else {
+        return max(nums1left,nums2left);
+      }
+    } else if (nums1left > nums2right) {
+      high = nums1partition-1;
+    } else if (nums2left > nums1right) {
+      low = nums1partition +1;
+    }
+  }
+  return 0;
+}
+
+int main() {
+  vector<int> first = {1,3};
+  vector<int> second = {2};
+  double ans = findMedianSortedArrays(first, second);
+  cout << ans;
+}
+
+// double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
+//   if (nums1.size() > nums2.size()) {
+//     vector<int> temp = nums1;
+//     nums1 = nums2;
+//     nums2 = temp;
+//   }
+//   int lo = 0;
+//   int hi = nums1.size();
+//   int te = nums1.size() + nums2.size();
+//   while (lo <= hi) {
+//     int aleft = (lo + hi) / 2;
+//     int bleft = (te + 1) / 2 - aleft;
+
+//     int alm1 = (aleft == 0) ? INT_MIN : nums1[aleft - 1];
+//     int a1 = (aleft == nums1.size()) ? INT_MAX : nums1[aleft];
+//     int blm1 = (bleft == 0) ? INT_MIN : nums2[bleft - 1];
+//     int b1 = (bleft == nums2.size()) ? INT_MAX : nums2[bleft];
+
+//     if (alm1 <= b1 && blm1 <= a1) {
+//       double median = 0.0;
+//       if (te % 2 == 0) {
+//         int lmax = max(alm1, blm1);
+//         int rmin = min(a1, b1);
+//         median = (lmax + rmin) / 2.0;
+//       } else {
+//         int lmax = max(alm1, blm1);
+//         median = lmax;
+//       }
+//       return median;
+//     } else if (alm1 > b1) {
+//       hi = aleft - 1;
+//     } else if (blm1 > a1) {
+//       lo = aleft + 1;
+//     }
+//   }
+//   return 0;
+// }
